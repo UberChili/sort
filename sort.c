9d763d201e4ac9d1ac027df413373ffdc15c72fd
Juan Andres Núñez <nunezlopez.ja@gmail.com>
2016-07-13 17:32:16 -0500

    Improved the main structure

--- sort.c
+++ sort.c
@@ -38,13 +38,19 @@ int main(void)
 
 	for (int i = 0; i < size; i++)
 	{
-		printf ("array[%i] = %i",i, haystack[i]);
+		printf ("array[%i] = %i", i, haystack[i]);
 		printf ("\n");
 	}
 	printf("\n");
 
 	// Prompt user for sorting algorithm to use
-	printf ("Select a sorting algorithm:\n");
+	printf ("Sorting algorithms available:\n");
+	printf ("1. Bubble Sort\n");
+	printf ("2. Insertion Sort\n");
+	printf ("3. Quick Sort\n");
+	printf ("4. Merge Sort\n");
+	printf ("\n");
+	printf ("Enter an algorithm to use: ");
 	int selection = GetInt();
 
 	// Making some more space
