59a8265b20f2713e2d0dd37a2de98199ef38bf27
Juan Andres Núñez <nunezlopez.ja@gmail.com>
2016-07-13 19:48:55 -0500

    Insertion sort presumably works now, need to test

--- helpers.c
+++ helpers.c
@@ -16,7 +16,23 @@ void heap(int values[], int n)
 
 void insertion(int values[], int n)
 {
-	// TODO
+	int j;
+
+	for (int i = 2; i < n; i++)
+	{
+		int key = values[i];
+
+		//insert values[i] into the sorted sequence values[1.. j - 1].
+		j = i - 1;
+
+		while ( j > 0 && values[j] > key)
+		{
+			values[j + 1] = values[j];
+			j = j - 1;
+		}
+
+		values[j + 1] = key;
+	}
 }
 
 void quick(int values[], int n)
